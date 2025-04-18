4cabb3ceaef5c162b48c84db4f2f92a5e83e7fc8
yeonseo0612 <qnq11@naver.com>
2025-04-18 18:21:58 +0900

    Update TestMain.java

--- ex_0415/src/test/TestMain.java
+++ ex_0415/src/test/TestMain.java
@@ -1,7 +1,9 @@
 package test;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -45,6 +47,85 @@ public class TestMain {
         for(int i=0; i<number.length(); i++) {
         	
         }
+        
+        String my_string = "rermgorpsam";
+        int[][] queries = {{2, 3}, {0, 7}, {5, 9}, {6, 10}};
+        
+        for(int[] query : queries) {
+        	int s = query[0];
+        	int e = query[1];
+        	String word = "";
+        	for(int i= e; i>= s; i--) {
+        		word += my_string.charAt(i); 
+        	}
+        	String start = my_string.substring(0,s);
+        	String end = my_string.substring(e+1);
+        	
+        	my_string = start + word + end;
+        };
+        
+//        int[] num_list = {4,2,6,1,7,6};
+//        int[] answer = {};
+//        int n = 2;
+//        
+//        if(num_list.length % 2 == 0){
+//            answer = new int[num_list.length / n];
+//        } else{
+//            answer = new int[num_list.length / n +1];
+//        }
+//        
+//        int index = 0;
+//        
+//        for(int i=0; i<num_list.length; i+=n){ 
+//            answer[index++] = num_list[i];
+//        }
+//        
+//        Arrays.sort(num_list);
+//        System.out.println(Arrays.toString(num_list));
+        
+        
+        
+        String[] my_strings = {"progressive", "hamburger", "hammer", "ahocorasick"};
+        int[][] parts = {{0, 4}, {1, 2}, {3, 5}, {7, 7}};
+        
+        String answer = "";
+        List<String> list = new ArrayList<>();
+        
+    	for(int i=0; i<my_strings.length; i++){
+            list.add(my_strings[i].substring(parts[i][0],parts[i][1]+1));
+        }
+        
+  
+        for(int j=0; j<list.size(); j++) {
+        	answer += list.get(j);
+        }
+        
+        System.out.println(answer);
+        
+        my_string = "ProgrammerS123";
+        answer = my_string.substring(my_string.length()-11);
+        System.out.println(answer);
+        
+        my_string = "xxxxixxxx";
+        
+        System.out.println(Arrays.toString(my_string.split("x")));
+        
+        int[] a = {};
+        String[] s = my_string.split("x",-1);
+        
+        System.out.println(Arrays.toString(s));
+        
+        if(my_string.endsWith("x")){
+            a = new int[s.length+1];
+        }else{
+            a = new int[s.length];
+        }
+        
+        for(int i=0; i<my_string.split("x").length; i++) {
+        	a[i] = s[i].length();
+        }
+        System.out.println(Arrays.toString(a));
+       
         
 		
 	}
