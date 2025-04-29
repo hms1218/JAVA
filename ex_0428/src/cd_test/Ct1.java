c330088f765da4da987f3f7bde1dac643badda0e
hms1218 <rhkwmq93@naver.com>
2025-04-29 18:20:03 +0900

    푸는중

---
+++ ex_0428/src/cd_test/Ct1.java
@@ -0,0 +1,27 @@
+package cd_test;
+
+public class Ct1 {
+	public static void main(String[] args) {
+		//공원
+		int[] mats = {5,3,2}; //돗자리 한변의 길이
+		String[][] park = {
+				{"A" , "A" , "-1", "B" , "B" , "B" , "B" , "-1"}, 
+				{"A" , "A" , "-1", "B" , "B" , "B" , "B" , "-1"}, 
+				{"-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1"}, 
+				{"D" , "D" , "-1", "-1", "-1", "-1", "E" , "-1"}, 
+				{"D" , "D" , "-1", "-1", "-1", "-1", "-1", "F" }, 
+				{"D" , "D" , "-1", "-1", "-1", "-1", "E" , "-1"}
+			};
+		int answer = 0;
+		int count = 0;
+		
+		for(int i=0; i<park.length; i++) {
+			for(int j=0; j<park[i].length; j++) {
+				
+			}
+		}
+		
+		
+		
+	}
+}
