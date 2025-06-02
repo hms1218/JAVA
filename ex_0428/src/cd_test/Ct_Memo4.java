8b5439a6cef9531a804c00ce3fef5d6bcda19a1d
hms1218 <rhkwmq93@naver.com>
2025-06-02 18:21:53 +0900

    Create Ct_Memo4.java

---
+++ ex_0428/src/cd_test/Ct_Memo4.java
@@ -0,0 +1,13 @@
+package cd_test;
+
+public class Ct_Memo4 {
+	public static void main(String[] args) {
+		int[] rank = {3,7,2,5,4,6,1};
+		boolean[] attendance = {false, true, true, true, true, false, false};
+		int answer = 0;
+		
+		for(int i=0; i<rank.length; i++) {
+			
+		}
+	}
+}
